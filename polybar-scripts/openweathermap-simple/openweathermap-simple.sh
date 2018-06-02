03f0430de30887e2e7e798119ad9d4bfc98d237f
Davi Alexandre <davi@davialexandre.com.br>
2018-06-02 14:14:14 -0300

    Openweathermap-simple customizations
    - Use nerd-fonts icons
    - Set city and unit
    - Read APP_ID from configuration file

--- polybar-scripts/openweathermap-simple/openweathermap-simple.sh
+++ polybar-scripts/openweathermap-simple/openweathermap-simple.sh
@@ -1,33 +1,34 @@
 #!/bin/sh
 
+# Openweathermap icons reference: https://openweathermap.org/weather-conditions
 get_icon() {
     case $1 in
-        01d) icon="";;
+        01d) icon="";;
         01n) icon="";;
-        02d) icon="";;
+        02d) icon="";;
         02n) icon="";;
-        03*) icon="";;
-        04*) icon="";;
-        09d) icon="";;
-        09n) icon="";;
-        10d) icon="";;
-        10n) icon="";;
-        11d) icon="";;
-        11n) icon="";;
-        13d) icon="";;
-        13n) icon="";;
-        50d) icon="";;
-        50n) icon="";;
-        *) icon="";
+        03*) icon="";;
+        04*) icon="";;
+        09d) icon="";;
+        09n) icon="";;
+        10d) icon="";;
+        10n) icon="";;
+        11d) icon="";;
+        11n) icon="";;
+        13d) icon="";;
+        13n) icon="";;
+        50d) icon="";;
+        50n) icon="";;
+        *) icon="";
     esac
 
     echo $icon
 }
 
-KEY=""
-CITY=""
+KEY=$(cat ~/openweathermap_appid)
+CITY="3448439"
 UNITS="metric"
-SYMBOL="°"
+SYMBOL="°C"
 
 if [ ! -z $CITY ]; then
     weather=$(curl -sf "http://api.openweathermap.org/data/2.5/weather?APPID=$KEY&id=$CITY&units=$UNITS")
